80e0442ad791b1e3acedcca113be9893806a0a10
Meg <41833919+SpicyCayenne@users.noreply.github.com>
2022-01-31 10:57:20 -0800

    added responsive styles and more fun facts

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ let funFacts;
 let oldFunFacts = [];
 
 funFactsList = ['My first "real" experience with programming was using VBA in Excel. I automated a lot of my data-entry tasks for work (shhh!)',
-"I'm an old lady at heart; I love knitting, gardening, and cooking!",'My favorite place to visit is Crater Lake National Park.'];
+"I'm an old lady at heart; I love knitting, gardening, and cooking!",'My favorite place to visit is Crater Lake National Park.',
+'I had to take Calculus three times before I finally understood it. Persistance pays off!', 'I am a Viking 3 times over: my undergraduate and post-baccalaureate mascots were both vikings, and I learned through DNA testing that I am of Norwegian descent. Skål!',
+'I have a terrible sense of smell.', 'My favorite subject in high school was band. I played 4 instruments: piano, flute, guitar, euphonium.', 'My favorite characters to play in D&D are rogues and clerics.'];
 
 
 function funFact() {
     let randInt = Math.floor(Math.random()*(funFactsList.length));
     while (oldFunFacts.indexOf(randInt) !== -1) {
+        //to prevent seeing duplicates until you've seen the whole list
         randInt = Math.floor(Math.random()*(funFactsList.length));
     }
     oldFunFacts.push(randInt);
-    console.log(oldFunFacts)
     document.getElementById('funFact').innerText = funFactsList[randInt];
     if (oldFunFacts.length === funFactsList.length) {
         oldFunFacts = [];
     }
     document.getElementById('funFact').style.display = 'block';
+    document.getElementById('funFact').scrollIntoView();
 }
